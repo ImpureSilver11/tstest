f90c78abe36dd384969b00220780ba0b610e3ddd
MasahiroTakahashi <take1931032@gmail.com>
2019-07-14 15:09:00 +0900

    doing

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # tstest
 
 #### References
+
 実践TypeScript https://www.amazon.co.jp/dp/B07T477V6G/ref=cm_sw_r_tw_dp_U_x_QXRkDbFVS2ZB2
 
+一部の部分をこれでやる。二部は別でPullRequestつくる
+<!-- 一章done -->
+<!-- TODO:二章やる -->
 
 ## command
 
@@ -14,3 +18,9 @@ tsc --init
 
 #### compile
 tsc
+
+#### build 
+tsc -b PATH
+
+#### build-tool
+https://parceljs.org/
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,4 @@
+// ~ 1-2-10
 // // デフォルトany型
 // export function test(arg){
 //   let hoge = null
@@ -5,9 +6,17 @@
 //   return hoge
 // }
 
-export function test1(){
-  return 'test1'
-}
-export function test2(){
-  return { value: 'test2' }
-}
+// ~ 1-2-14
+// export function test1(){
+//   return 'test1'
+// }
+// export function test2(){
+//   return { value: 'test2' }
+// }
+
+// 1-2-17
+// ライブラリ型定義の利用 https://definitelytyped.org/
+import { sampleText, sampleFunction } from './sample'
+
+const a = sampleFunction
+const b = sampleText
